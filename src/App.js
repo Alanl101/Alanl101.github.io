562b1890ecdfe55fcdbf632c0248f706ddb85175
Alan Lopez <70230548+Alanl101@users.noreply.github.com>
2022-09-20 12:44:11 -0500

    First addition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Popup from './components/Popup';
+import { useState } from 'react';
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  
+  //List of Dealerships and check mark tracker
+  const [checked, setChecked] = useState([]);
+  const checkList = ["Dealership 1", "Dealership 2"];
+
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  const checkedItems = checked.length
+    ? checked.reduce((total, item) => {
+        return total + ", " + item;
+      })
+    : "";
+
+    
+  var isChecked = (item) =>
+  checked.includes(item) ? "checked-item" : "not-checked-item";
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main>
+      <h1>React Popup</h1>
+      <div className='checkList'>
+        <div className='title'>Dealerships</div>
+        <div className='list-container'>
+          {checkList.map((item,index) => (
+              <div key={index}>
+                <input value={item} type="checkbox" onChange={handleCheck} />
+                <span className={isChecked(item)}>{item}</span>
+              </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        {`Items checked are: ${checkedItems}`}
+      </div>
+
+        
+      <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+      
+
+      </main>
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>My Popup</h3>
+        <div>
+        {`Items checked are: ${checkedItems}`}
+        </div>
+      </Popup>
     </div>
+
+
   );
 }
 
---
+++ src/README.md
@@ -0,0 +1,4 @@
+# Dealership-Application
+
+Our Goal: Create a web app that could keep track of names that have been checked off and then display them inside of a pop up window
+
